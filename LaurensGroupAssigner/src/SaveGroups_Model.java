2a30665936b97fc7a48956d2475450397078475f
Shane-Ondriezek <ondriezek.shane@gmail.com>
2019-01-18 13:47:16 -0500

    Refactor to accept a boolean parameter
    The boolean, isFirstRotation, is used by the method to deteremine
    whether the data should overwrite the new file (which it does on the
    first rotation), or be appended (which happens on any other rotation).

--- LaurensGroupAssigner/src/SaveGroups_Model.java
+++ LaurensGroupAssigner/src/SaveGroups_Model.java
@@ -1,5 +1,5 @@
-//accepts an arrayList of HashMaps, where hashmap is a group
-//saves every activity name and that activitie's students to the current
+//accepts an arrayList<HashMap>, where each hashmap is a group
+//saves every activity name and that activity's students to the current
 //directory in the "activityGroups.txt" file
 
 import java.io.BufferedWriter;
@@ -8,79 +8,105 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 public class SaveGroups_Model {
+
     final static String FILENAME = "activityGroups.txt";
     final static File GROUPFILE = new File(FILENAME);
-    
-    public static void saveGroups(ArrayList<HashMap> groupMaps) {
-            //groups in groupMaps are in this order:
-            //readToSelf
-            //readToSomeone
-            //wordWork
-            //workOnWriting
-            //listenToReading
-            
-            ArrayList<String> readToSelfList;
-            ArrayList<String> readToSomeoneList;
-            ArrayList<String> wordWorkList;
-            ArrayList<String> workOnWritingList;
-            ArrayList<String> listenToReadingList;
-            
-            FileWriter fWriter = null;
+    static boolean isFirstRotation;
+
+    public static void saveGroups(ArrayList<HashMap> groupMaps,
+            boolean isFirstRotation) {
+        //groups in groupMaps are in this order:
+        //readToSelf
+        //readToSomeone
+        //wordWork
+        //workOnWriting
+        //listenToReading
+
+        SaveGroups_Model.isFirstRotation = isFirstRotation;
+        System.out.println(isFirstRotation);
+        ArrayList<String> readToSelfList;
+        ArrayList<String> readToSomeoneList;
+        ArrayList<String> wordWorkList;
+        ArrayList<String> workOnWritingList;
+        ArrayList<String> listenToReadingList;
+
+        FileWriter fWriter = null;
         try {
-            fWriter = new FileWriter(GROUPFILE);
+            if (isFirstRotation) {
+                //overwrite for the first rotation
+                fWriter = new FileWriter(GROUPFILE);
+            } else {
+                //append to the file after first rotation
+                fWriter = new FileWriter(GROUPFILE, true);
+            }
+
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-            BufferedWriter bWriter = new BufferedWriter(fWriter);
-            
-            for (int i = 0; i < groupMaps.size(); i++) {
-                switch (i) {
-                    case 0:
-                        readToSelfList = 
-                                new ArrayList<>(groupMaps.get(0).values());
-                        writeNamesForGroup(readToSelfList, "Read to Self",
-                                bWriter);
-                        break;
-                    case 1:
-                        readToSomeoneList = 
-                                new ArrayList<>(groupMaps.get(1).values());
-                        writeNamesForGroup(readToSomeoneList, "Read to Someone",
-                                bWriter);
-                        break;
-                    case 2:                      
-                        wordWorkList = 
-                                new ArrayList<>(groupMaps.get(2).values());
-                        writeNamesForGroup(wordWorkList, "Word Work",
-                                bWriter);
-                        break;
-                    case 3:
-                        workOnWritingList = 
-                                new ArrayList<>(groupMaps.get(3).values());
-                        writeNamesForGroup(workOnWritingList, "Work On Writing",
-                                bWriter);
-                        break;
-                    case 4:
-                        listenToReadingList = 
-                                new ArrayList<>(groupMaps.get(4).values());
-                        writeNamesForGroup(listenToReadingList, "Listen to Reading",
-                                bWriter);
-                        break;
-                }
+        
+        
+        BufferedWriter bWriter = new BufferedWriter(fWriter);
+        if (isFirstRotation) {
+            try {
+                bWriter.write("FIRST ROTATION\n");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            try {
+                bWriter.write("\n\n\n\nNEXT ROTATION\n");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        
+        for (int i = 0; i < groupMaps.size(); i++) {
+            switch (i) {
+                case 0:
+                    readToSelfList
+                            = new ArrayList<>(groupMaps.get(0).values());
+                    writeNamesForGroup(readToSelfList, "Read to Self",
+                            bWriter);
+                    break;
+                case 1:
+                    readToSomeoneList
+                            = new ArrayList<>(groupMaps.get(1).values());
+                    writeNamesForGroup(readToSomeoneList, "Read to Someone",
+                            bWriter);
+                    break;
+                case 2:
+                    wordWorkList
+                            = new ArrayList<>(groupMaps.get(2).values());
+                    writeNamesForGroup(wordWorkList, "Word Work",
+                            bWriter);
+                    break;
+                case 3:
+                    workOnWritingList
+                            = new ArrayList<>(groupMaps.get(3).values());
+                    writeNamesForGroup(workOnWritingList, "Work On Writing",
+                            bWriter);
+                    break;
+                case 4:
+                    listenToReadingList
+                            = new ArrayList<>(groupMaps.get(4).values());
+                    writeNamesForGroup(listenToReadingList, "Listen to Reading",
+                            bWriter);
+                    break;
+            }
 
-            }  
+        }
         try {
             bWriter.flush();
             bWriter.close();
         } catch (IOException ex) {
-            Logger.getLogger(SaveGroups_Model.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
         }
     }
-    
-    private static void writeNamesForGroup(ArrayList<String> group, 
+
+    private static void writeNamesForGroup(ArrayList<String> group,
             String activityName, BufferedWriter bWriter) {
         try {
             bWriter.write(activityName);
